050cd777f9c5941e52580f15f39efc856d3dec39
Ivan2567 <78481838+Ivan2567@users.noreply.github.com>
2022-03-08 22:23:56 +0300

    Add files via upload

---
+++ lab1/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id='app'>
+        <h2>{{ myname}}</h2>
+        <vtsu :page = "page"></vtsu>
+        <regform></regform>
+    </div>
+    
+</template>
+
+<script lang="ts">
+//import Vue from 'vue';
+import vtsu  from '../../vt-su/src/components/vtsu.vue'
+import regform from '../../regform/src/components/regf.vue'
+declare var require: any;
+import { defineComponent } from 'vue';
+import axios from 'axios';
+var url = 'https://yandex.ru/';
+export default defineComponent({
+name: 'App',
+components: {vtsu, regform},
+data(){
+  return{
+      myname: require('get-my-myname'),page:[]
+      };
+    },
+    mounted(){
+      axios
+      .get(url,
+      { headers: {"Access-Control-Allow-Origin" : "https://yandex.ru/" },
+      withCredentials: false,}
+    ).then((response: any) =>
+        {this.page = response.data;},
+        ); 
+  }, 
+  });
+</script> 
