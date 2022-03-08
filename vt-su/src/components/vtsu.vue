238254fbea320e81fbdb85e2a4ed7f07dabcbaab
Ivan2567 <78481838+Ivan2567@users.noreply.github.com>
2022-03-08 22:31:49 +0300

    Add files via upload

---
+++ vt-su/src/components/vtsu.vue
@@ -0,0 +1,16 @@
+<template>
+ <iframe :srcdoc="page" class="frame" id="frame"></iframe>
+</template>
+<script lang="ts">
+export default {
+  props: {
+    page: [],
+  },
+};
+</script>
+<style>
+.frame{
+    width: 100%;
+    height: 600px;
+    }
+</style>
